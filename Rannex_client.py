0a16951e73d63e7ace3164f4a12e33f6c0917ec7
RANVEER-VERMA <63558842+RANVEER-VERMA@users.noreply.github.com>
2020-04-15 00:05:06 +0530

    Updated

--- Rannex_client.py
+++ Rannex_client.py
@@ -2,5 +2,5 @@ import socket
 c = socket.socket()
 c = connect(('localhost',9999))
 name = input("Please enter your name:-")
-c.send(bytes(name, utf-8'))
+c.send(bytes(name,'utf-8'))
 print(c.recv(1024).decode())
